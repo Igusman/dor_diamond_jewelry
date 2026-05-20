759acbad3961eae2899bbfa9aa59a40afbea4a79
igusman <igusman018@gmail.com>
2026-05-20 09:35:08 +0300

    Remove About nav link from header

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -2,14 +2,9 @@ function SiteHeader() {
   return (
     <header className="top-bar">
       <p className="brand-mark">dor_diamond_jewelry</p>
-      <nav className="top-links" aria-label="ניווט מהיר">
-        <a className="ghost-link" href="#about">
-          אודות
-        </a>
-        <a className="ghost-link" href="#collections">
-          לצפייה בקולקציה
-        </a>
-      </nav>
+      <a className="ghost-link" href="#collections">
+        לצפייה בקולקציה
+      </a>
     </header>
   )
 }
